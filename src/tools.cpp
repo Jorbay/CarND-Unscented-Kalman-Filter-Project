5dd0f0c6abb3b654b3b71c30b9e6151e8459eee2
Jorge <jaorbay@gmail.com>
2018-04-01 17:28:46 -0700

    Importing RMSE function from previous term 2 project 1

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,31 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  
+  if(estimations.size() != ground_truth.size() 
+	|| estimations.size() == 0){
+	
+	cout << "Invalid estimation or ground_truth data" << endl;
+	return rmse;
+  }
+
+  //Calculating differences between corresponding vectors squared and adding results to rmse vector
+  for(unsigned int i=0; i < estimations.size(); ++i){
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+      residual = residual.array()*residual.array();
+      rmse += residual;
+  }
+
+  //calculating the mean from the total differences squared
+  rmse = rmse/estimations.size();
+
+  //calculating the squared root
+  rmse = rmse.array().sqrt();
+
+  //returning the result
+  return rmse;
 }
